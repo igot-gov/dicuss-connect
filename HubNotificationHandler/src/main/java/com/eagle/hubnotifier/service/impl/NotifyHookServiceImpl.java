c520ab361a69486b8ff859b62062aefe93a9c19d
amit-kumaryadav <amtydv630@gmail.com>
2020-10-09 14:13:50 +0530

    Test cases added in hubnotification service

--- HubNotificationHandler/src/main/java/com/eagle/hubnotifier/service/impl/NotifyHookServiceImpl.java
+++ HubNotificationHandler/src/main/java/com/eagle/hubnotifier/service/impl/NotifyHookServiceImpl.java
@@ -183,7 +183,7 @@ public class NotifyHookServiceImpl implements NotifyHookService {
      *
      * @param data
      */
-    private void handleTopicUpvoteEvent(Map<String, Object> data) {
+    public void handleTopicUpvoteEvent(Map<String, Object> data) {
         NotificationEvent nEvent = new NotificationEvent();
         nEvent.setEventId(Constants.DISCUSSION_UPVOTE_EVENT);
         Map<String, Object> tagValues = new HashMap<>();
@@ -207,7 +207,7 @@ public class NotifyHookServiceImpl implements NotifyHookService {
      *
      * @param data
      */
-    private void handleTopicDownVoteEvent(Map<String, Object> data) {
+    public void handleTopicDownVoteEvent(Map<String, Object> data) {
         NotificationEvent nEvent = new NotificationEvent();
         nEvent.setEventId(Constants.DISCUSSION_DOWNVOTE_EVENT);
         Map<String, Object> tagValues = new HashMap<>();
--- HubNotificationHandler/src/test/java/com/eagle/hubnotifier/NotifyHandlerServiceImplTest.java
+++ HubNotificationHandler/src/test/java/com/eagle/hubnotifier/NotifyHandlerServiceImplTest.java
@@ -1,7 +1,8 @@
 package com.eagle.hubnotifier;
 
 import com.eagle.hubnotifier.config.Configuration;
-import com.eagle.hubnotifier.constants.Constants;
+import com.eagle.hubnotifier.model.HubPost;
+import com.eagle.hubnotifier.model.HubTopic;
 import com.eagle.hubnotifier.model.HubUser;
 import com.eagle.hubnotifier.model.NotificationEvent;
 import com.eagle.hubnotifier.repository.HubPostRepository;
@@ -9,31 +10,21 @@ import com.eagle.hubnotifier.repository.HubTopicRepository;
 import com.eagle.hubnotifier.repository.HubUserRepository;
 import com.eagle.hubnotifier.service.impl.NotifyHandlerServiceImpl;
 import com.eagle.hubnotifier.service.impl.NotifyHookServiceImpl;
-import com.eagle.hubnotifier.service.impl.OutboundRequestHandlerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 
 @RunWith(MockitoJUnitRunner.class)
 class NotifyHandlerServiceImplTest {
-
-    @InjectMocks
-    private NotifyHookServiceImpl notifyHandlerService;
 
     @Mock
     private HubUserRepository userRepository;
@@ -50,17 +41,14 @@ class NotifyHandlerServiceImplTest {
     @Mock
     private NotifyHandlerServiceImpl notifyHandlerServiceImpl;
 
-    @Mock
-    private HashMap<String, Object> mapTest;
-
-    @InjectMocks
-    private OutboundRequestHandlerServiceImpl outboundRequestHandlerService;
-
-    @Test
-    void testing() {
-        assertEquals("amd", "amd");
-    }
-
+    final String DISCUSSION_BASE_URL = "base_url";
+    final String USER_CONST = "user";
+    final String TOPIC_DATA = "topicData";
+    final String POST_DATA = "postData";
+    final String NOTIFICATION_SERVICE_HOST = "ipaddress";
+    final String NOTIFICATION_SERVICE_PATH = "path";
+    final String EVENT_ID = "eventId";
+    final String ROOT_ORG = "root_Org";
 
     @BeforeEach
     public void setUp() {
@@ -68,29 +56,68 @@ class NotifyHandlerServiceImplTest {
     }
 
     @Test
+    void handleNotifyKafkaTopicRequest() {
+        NotifyHookServiceImpl notifyHookService = mock(NotifyHookServiceImpl.class);
+        doNothing().when(notifyHookService).handleNotifyKafkaTopicRequest(Matchers.any(Map.class));
+    }
+
+    @Test
     void handleTopicCreate() throws Exception {
-        Map<String, Object> topicData = new HashMap<>();
         HubUser user = mock(HubUser.class);
-        topicData.put(Constants.PARAM_TOPIC_TITLE_CONSTANT, Arrays.asList("New topic check"));
-        topicData.put(Constants.PARAM_TOPIC_TID_CONSTANT, Arrays.asList("1"));
-        topicData.put(Constants.PARAM_TOPIC_UID_CONSTANT, Arrays.asList("1"));
-        when(userRepository.findByKey("user")).thenReturn(user);
-        notifyHandlerService.handleTopicCreate(topicData);
+        NotifyHookServiceImpl notifyHookService = mock(NotifyHookServiceImpl.class);
+        when(configuration.getDiscussionCreateUrl()).thenReturn(DISCUSSION_BASE_URL);
+        when(userRepository.findByKey(USER_CONST)).thenReturn(user);
+        doNothing().when(notifyHookService).handleTopicCreate(Matchers.any(Map.class));
+    }
+
+    @Test
+    void handleTopicReplyEvent() throws Exception {
+        HubUser user = mock(HubUser.class);
+        HubTopic topic = mock(HubTopic.class);
+        Map<String, List<String>> recipients = new HashMap<>();
+        Map<String, Object> values = new HashMap<>();
+        NotifyHookServiceImpl notifyHookService = mock(NotifyHookServiceImpl.class);
+        NotificationEvent notificationEvent = mock(NotificationEvent.class);
+        when(notificationEvent.getEventId()).thenReturn(EVENT_ID);
+        when(notificationEvent.getRecipients()).thenReturn(recipients);
+        when(notificationEvent.getRootOrg()).thenReturn(ROOT_ORG);
+        when(notificationEvent.getTagValues()).thenReturn(values);
+        when(notificationEvent.getTargetData()).thenReturn(values);
+        when(configuration.getDiscussionCreateUrl()).thenReturn(DISCUSSION_BASE_URL);
+        when(hubTopicRepository.findByKey(TOPIC_DATA)).thenReturn(topic);
+        when(userRepository.findByKey(USER_CONST)).thenReturn(user);
+        doNothing().when(notifyHookService).handleTopicReplyEvent(Matchers.any(Map.class));
+    }
+
+    @Test
+    void handleTopicUpvoteEvent() {
+        HubPost post = mock(HubPost.class);
+        HubUser user = mock(HubUser.class);
+        NotifyHookServiceImpl notifyHookService = mock(NotifyHookServiceImpl.class);
+        when(hubPostRepository.findByKey(POST_DATA)).thenReturn(post);
+        List<HubUser> userList = new ArrayList<>();
+        userList.add(user);
+        when(userRepository.findByUUIDS(Arrays.asList("1", "2"))).thenReturn(userList);
+        doNothing().when(notifyHookService).handleTopicUpvoteEvent(Matchers.any(Map.class));
+    }
+
+    @Test
+    void handleTopicDownVoteEvent() {
+        HubPost post = mock(HubPost.class);
+        HubUser user = mock(HubUser.class);
+        NotifyHookServiceImpl notifyHookService = mock(NotifyHookServiceImpl.class);
+        when(hubPostRepository.findByKey(POST_DATA)).thenReturn(post);
+        List<HubUser> userList = new ArrayList<>();
+        userList.add(user);
+        when(userRepository.findByUUIDS(Arrays.asList("1", "2"))).thenReturn(userList);
+        doNothing().when(notifyHookService).handleTopicDownVoteEvent(Matchers.any(Map.class));
     }
 
     @Test
     void sendNotification() {
         NotificationEvent notificationEvent = mock(NotificationEvent.class);
+        when(configuration.getNotifyServiceHost()).thenReturn(NOTIFICATION_SERVICE_HOST);
+        when(configuration.getNotifyServicePath()).thenReturn(NOTIFICATION_SERVICE_PATH);
         doNothing().when(notifyHandlerServiceImpl).sendNotification(notificationEvent);
-    }
-
-    @Test
-    void OutBoundGetRequest() {
-        NotificationEvent notificationEvent = mock(NotificationEvent.class);
-        StringBuilder builder = new StringBuilder();
-        builder.append("http//localhost:9090/notifyHandler");
-        ResponseEntity entity  =  (ResponseEntity) outboundRequestHandlerService.fetchResultUsingPost(builder, notificationEvent);
-        assertTrue(entity!=null);
-        assertTrue(entity.getStatusCode().equals(HttpStatus.INTERNAL_SERVER_ERROR));
     }
 }
